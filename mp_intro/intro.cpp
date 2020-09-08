bfc07949afeabb1e8c0d86689820675b106f8c2b
wangshuo <shuow6@illinois.edu>
2020-09-07 22:10:18 -0500

    Update Part 2

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -7,21 +7,32 @@
 void rotate(std::string inputFile, std::string outputFile) {
   // TODO: Part 2
   cs225::PNG png;
-  png.readFromFile(inputFile);
-  cs225::PNG result(png);
-  for (unsigned x = 0; x < png.width(); x++) {
-    for (unsigned y = 0; y < png.height(); y++) {
-       cs225::HSLAPixel & pixel = png.getPixel(x, y);
-       cs225::HSLAPixel & resultPixel = result.getPixel(png.height() - x - 1, png.width() - y - 1);
-       resultPixel = pixel;
-    }
-  }
+  if (png.readFromFile(inputFile)) {
+    cs225::PNG result(png);
+    for (unsigned int i = 0; i < png.width(); i++) {
+      for (unsigned int j = 0; j < png.height(); j++) {
+        cs225::HSLAPixel & resultPixel = result.getPixel(i, j);
+        resultPixel = png.getPixel(png.width() - 1 - i, png.height() - 1 - j);
+        }
+      }
   result.writeToFile(outputFile);
+  }
 }
 
 cs225::PNG myArt(unsigned int width, unsigned int height) {
   cs225::PNG png(width, height);
   // TODO: Part 3
-
+  for (unsigned x = 0; x < width; x++) {
+    for (unsigned y = 0; y < height; y++) {
+      cs225::HSLAPixel & pixel = png.getPixel(x, y);
+      if (x < 360) {
+        pixel.h = x;
+      } else {
+        pixel.h = y / 10;
+      }
+      pixel.l = 0.5;
+      pixel.s = 0.5;
+    }
+  }
   return png;
 }
