6c504df8602047419cb764e0b403a7994bdcdce7
wangshuo <shuow6@illinois.edu>
2020-09-09 11:59:57 -0500

    Finish Lab_debug

--- lab_debug/sketchify.cpp
+++ lab_debug/sketchify.cpp
@@ -12,10 +12,8 @@ using namespace cs225;
 
 /**
  * Creates a new output image.
-
  * @param w width of the image
  * @param h height of the image
-
  * @return a pointer to the newly-created image
  */
 PNG* setupOutput(unsigned w, unsigned h) {
@@ -25,33 +23,33 @@ PNG* setupOutput(unsigned w, unsigned h) {
 
 /**
  * Used to customize the color of the sketchified image.
-
  * @return a pointer to the color to use when sketchifying
  */
 HSLAPixel* myFavoriteColor() {
-    HSLAPixel p(-1, 0.8, 0.5);
-    return &p;
+    HSLAPixel* p = new HSLAPixel(127, 0.8, 0.5);
+    return p;
 }
 
 void sketchify(std::string inputFile, std::string outputFile) {
     // Load in.png
-    PNG* original = NULL;
+    PNG* original = new PNG();
 
     original->readFromFile(inputFile);
     unsigned width = original->width();
     unsigned height = original->height();
 
     // Create out.png
-    PNG* output;
-    setupOutput(width, height);
+    PNG* output = new PNG();
+    output = setupOutput(width, height);
 
     // Load our favorite color to color the outline
-    HSLAPixel* myPixel = myFavoriteColor();
+    HSLAPixel* myPixel = new HSLAPixel();
+    myPixel = myFavoriteColor();
 
     // Go over the whole image, and if a pixel differs from that to its upper
     // left, color it my favorite color in the output
-    for (unsigned y = 1; 0 < y < height; y++) {
-        for (unsigned x = 1; 0 < x < width; x++) {
+    for (unsigned y = 1; y < height; y++) {
+        for (unsigned x = 1; x < width; x++) {
             // Calculate the pixel difference
             HSLAPixel& prev = original->getPixel(x - 1, y - 1);
             HSLAPixel& curr = original->getPixel(x, y);
@@ -59,7 +57,7 @@ void sketchify(std::string inputFile, std::string outputFile) {
 
             // If the pixel is an edge pixel,
             // color the output pixel with my favorite color
-            HSLAPixel currOutPixel = (*output).getPixel(x, y);
+            HSLAPixel& currOutPixel = (*output).getPixel(x, y);
             if (diff > 20) {
                 currOutPixel = *myPixel;
             }
