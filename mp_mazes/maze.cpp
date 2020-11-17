a4eb19b8953508286ac38fdfa22e86506b15a025
wangshuo <shuow6@illinois.edu>
2020-11-17 01:06:31 -0600

    Improve runtime

--- mp_mazes/maze.cpp
+++ mp_mazes/maze.cpp
@@ -4,12 +4,15 @@
 #include <stdlib.h>
 #include <queue>
 #include <set>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
 
 SquareMaze::SquareMaze(){
 }	
 
 void SquareMaze::makeMaze(int width, int height){
-    _height = height;
+     _height = height;
     _width =  width;
     if(!walls.empty()){
         walls.clear();
@@ -25,59 +28,41 @@ void SquareMaze::makeMaze(int width, int height){
             walls[i].push_back(std::make_pair(true, true));
         }
     }
-    std::vector<int> wall_cells;
+    std::vector<std::pair<int, int>> wall_cells;
     for (int i = 0; i < width * height - 1; i++){
-        wall_cells.push_back(i);
+        wall_cells.push_back(std::make_pair(i, 0));
+        wall_cells.push_back(std::make_pair(i, 1));
     }
-    auto rng = std::default_random_engine{};
-    while(!wall_cells.empty()){
-        std::shuffle(wall_cells.begin(), wall_cells.end(), rng);
-        for(size_t i = 0; i < wall_cells.size(); i++){
-            int x = wall_cells[i] % width;
-            int y = wall_cells[i] / width;
-            int idx = wall_cells[i];
-            if(y == height - 1){
-                if(maze.find(idx) != maze.find(idx + 1)){
-                    walls[y][x].second = false;
-                    maze.setunion(idx, idx + 1);
-                }
-                wall_cells.erase(wall_cells.begin() + i);
-                i--;
-            }else if(x == width - 1){
-                if(maze.find(idx) != maze.find(idx + width)){
-                    walls[y][x].first = false;
-                    maze.setunion(idx, idx + width);
-                }
-                wall_cells.erase(wall_cells.begin() + i);
-                i--;
-            }else{
-                if(maze.find(idx) == maze.find(idx + 1) &&  maze.find(idx) == maze.find(idx + width)){
-                    wall_cells.erase(wall_cells.begin() + i);
-                    i--;
-                }else if(maze.find(idx) == maze.find(idx + 1)){
-                    walls[y][x].first = false;
-                    maze.setunion(idx, idx + width);
-                    wall_cells.erase(wall_cells.begin() + i);
-                    i--;
-                }else if(maze.find(idx) == maze.find(idx + width)){
-                    walls[y][x].second = false;
-                    maze.setunion(idx, idx + 1);
-                    wall_cells.erase(wall_cells.begin() + i);
-                    i--;
-                }else{
-                    int dir = rand() % 2;
-                    if(dir == 0){
-                        walls[y][x].second = false;
-                        maze.setunion(idx, idx + 1);
-                    }else{
-                        walls[y][x].first = false;
-                        maze.setunion(idx, idx + width);
-                    }
-                }
+
+    int count = 0;
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(wall_cells.begin(), wall_cells.end(), g);
+    for(size_t i = 0; i < wall_cells.size(); i++){
+        if (count == width * height - 1)
+            break;
+        int idx = wall_cells[i].first;
+        int dir = wall_cells[i].second;
+        int x = idx % width;
+        int y = idx / width;
+
+        if (dir == 0 && x != width-1) {
+            if (maze.find(idx) != maze.find(idx+1)) {
+                walls[y][x].second = false;
+                maze.setunion(idx, idx + 1);
+                count++;
+            }
+        }
+        else if (dir == 1 && y != height - 1) {
+            if(maze.find(idx) != maze.find(idx + width)){
+                walls[y][x].first = false;
+                maze.setunion(idx, idx + width);
+                count++;
             }
         }
     }
-}		
+}
+
 bool SquareMaze::canTravel(int x, int y, int dir) const{
     std::pair<bool, bool> wall = walls[y][x];
     if(dir == 1){
@@ -108,55 +93,100 @@ void SquareMaze::setWall(int x, int y, int dir, bool exists){
 }		
 
 std::vector<int> SquareMaze::solveMaze(){
-    std::queue<std::pair<std::pair<int, int>, std::vector<int>>> q;
-    std::set<std::pair<int, int>> visited;
-    visited.insert(std::make_pair(0, 0));
-    std::vector<int> path;
-    std::vector<int> curlongest;
-    int curlongest_x = 0;
-    q.push(std::make_pair(std::make_pair(0, 0), path));
-    while(!q.empty()){
-        std::pair<std::pair<int, int>, std::vector<int>> elem = q.front();
-        q.pop();
-        int x = elem.first.first;
-        int y = elem.first.second;
-        path = elem.second;
-        if(y == _height - 1){
-            if(path.size() > curlongest.size()){
-                curlongest = path;
-                curlongest_x = x;
-            }
-            if(path.size() == curlongest.size() && x < curlongest_x){
-                curlongest = path;
-                curlongest_x = x;
-            }
-        }
-        if(canTravel(x, y, 0) && visited.count(std::make_pair(x + 1, y)) == 0){
-            std::vector<int> right_path = path;
-            right_path.push_back(0);
-            q.push(std::make_pair(std::make_pair(x + 1, y), right_path));
-            visited.insert(std::make_pair(x + 1, y));
-        }
-        if(canTravel(x, y, 1) && visited.count(std::make_pair(x, y + 1)) == 0){
-            std::vector<int> down_path = path;
-            down_path.push_back(1);
-            q.push(std::make_pair(std::make_pair(x, y + 1), down_path));
-            visited.insert(std::make_pair(x, y + 1));
-        }
-        if(canTravel(x, y, 2) && visited.count(std::make_pair(x - 1, y)) == 0){
-            std::vector<int> left_path = path;
-            left_path.push_back(2);
-            q.push(std::make_pair(std::make_pair(x - 1, y), left_path));
-            visited.insert(std::make_pair(x - 1, y));
-        }
-        if(canTravel(x, y, 3) && visited.count(std::make_pair(x, y - 1)) == 0){
-            std::vector<int> up_path = path;
-            up_path.push_back(3);
-            q.push(std::make_pair(std::make_pair(x, y - 1), up_path));
-            visited.insert(std::make_pair(x, y - 1));
-        }
-    }
-    return curlongest;
+    std::vector< int > distance( _width*_height, -1);
+	std::vector< int > previous( _width*_height, -1);
+	std::queue< int > Q;
+
+	// Push all cells/vertices into the Queue
+	Q.push(0);
+
+	// Distance from source to source = 0
+	distance[ 0 ] = 0;
+
+	// Nodes that are unoptimized are in the queue
+	while ( !Q.empty() )
+	{
+		// Remove the vertex with the smallest distance
+		int u = Q.front();
+		Q.pop();
+
+		// For each neighbor of u, calculate distance to source
+		std::vector< int > neighbors;
+		neighbors.clear();
+		int x = u % _width;
+		int y = u / _width;
+		if ( canTravel( x, y, 0 ) )
+		{
+			neighbors.push_back( _width * y + x + 1 );
+		}
+		if ( canTravel( x, y, 1 ) )
+		{
+			neighbors.push_back( _width * (y+1) + x );
+		}
+		if ( canTravel( x, y, 2 ) )
+		{
+			neighbors.push_back( x-1 + y * _width );
+		}
+		if ( canTravel( x, y, 3 ) )
+		{
+			neighbors.push_back( x + _width*(y-1) );
+		}
+
+		for ( size_t i = 0; i < neighbors.size(); i++ )
+		{
+			int alt = distance[ u ] + 1;
+		if ( distance[ neighbors[ i ] ] == -1 )
+			{
+				distance[ neighbors[i] ] = alt;
+				previous[ neighbors[i] ] = u;
+                Q.push(neighbors[i]);
+			}
+		}
+	}
+
+	// Find the vertex along the bottom with the longest distance to source
+	int maxDistance = 0;
+	int maxDistanceVertex = 0;
+	for ( int i = _width * (_height-1); i < _width * _height; i++ )
+	{
+		if ( distance[i] > maxDistance )
+		{
+			maxDistance = distance[i];
+			maxDistanceVertex = i;
+		}
+	}
+
+	// Store in reverse the directions from source to target
+	std::vector< int > directions;
+
+	int v = maxDistanceVertex;
+	int curr = previous[ v ];
+	while ( curr != -1 )
+	{
+		if ( curr + 1 == v )
+		{
+			directions.push_back( 0 );
+		}
+		else if ( curr + _width == v  )
+		{
+			directions.push_back( 1 );
+		}
+		else if ( curr - 1 == v )
+		{
+			directions.push_back( 2 );
+		}
+		else if ( curr - _width == v  )
+		{
+			directions.push_back( 3 );
+		}
+		v = curr;
+		curr = previous[ curr ];
+	}
+
+	// Reverse the directions for the final set of directions
+	std::reverse( directions.begin(), directions.end() );
+
+	return directions;
 }	
 
 PNG * SquareMaze::drawMaze()const{
